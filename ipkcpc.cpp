27f672ead4d2397a1e971953ec8acb3ef7895b61
xmrkva04 <xmrkva04@noreply.%(DOMAIN)s>
2023-03-19 11:54:01 +0100

    Added basic structure of the code

---
+++ ipkcpc.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void arg_check(int argc, char * argv[]);
+void do_udp();
+void do_tcp();
+
+
+int main(int argc, char * argv[]) {
+    return 0;
+}
+
+void arg_check(int argc, char * argv[]){
+}
+
+void do_udp(){
+}
+
+void do_tcp(){
+}
