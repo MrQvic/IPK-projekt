99454a23bb5dbfce50d19c39aede63d428019bda
xmrkva04 <xmrkva04@noreply.%(DOMAIN)s>
2023-03-21 21:30:59 +0100

    Relase 1.1
    Upraveny komentáře, přidány citace a ERROR zprávy

--- ipkcpc.cpp
+++ ipkcpc.cpp
@@ -25,6 +25,7 @@ void leave(int s);
 int main(int argc, char * argv[]) {
 
   /*  Odchyceni vyjimky preruseni (ctrl-c) */
+  /*  inspirace z https://stackoverflow.com/questions/1641182/how-can-i-catch-a-ctrl-c-event*/
   struct sigaction interrupthandler;
   memset(&interrupthandler, 0, sizeof(struct sigaction));
   interrupthandler.sa_flags = 0;
@@ -38,7 +39,7 @@ int main(int argc, char * argv[]) {
 /* Funkce pro kontrolu zadaných argumentů */
 void arg_check(int argc, char * argv[]){
   if (argc != 7) {
-      fprintf(stderr,"usage: ipkcpc -h <host> -p <port> -m <mode>\n");
+      fprintf(stderr,"Pouziti: ./ipkcpc -h <host> -p <port> -m <mode>\n");
       exit(EXIT_FAILURE);
   }
   const char *server_name;
@@ -56,15 +57,15 @@ void arg_check(int argc, char * argv[]){
       mode = argv[i+1];
     
     else{
-      fprintf(stderr, "Wrong input arguments! %s\n", argv[i]);
-      fprintf(stderr, "usage: ipkcpc -h <host> -p <port> -m <mode>\n");
+      fprintf(stderr, "Spatne zadany vstupni argument %s\n", argv[i]);
+      fprintf(stderr, "Pouziti: ./ipkcpc -h <host> -p <port> -m <mode>\n");
       exit(EXIT_FAILURE);
     }
   }
 
   if(strcmp(mode, "udp") != 0 && strcmp(mode, "tcp") != 0){
-    fprintf(stderr, "Wrong input argument <mode>!\n");
-    fprintf(stderr, "usage: ipkcpc -h <host> -p <port> -m <mode>\n");
+    fprintf(stderr, "Spatne zadany argument <mode>!\n");
+    fprintf(stderr, "Pouziti: ./ipkcpc -h <host> -p <port> -m <mode>\n");
     exit(EXIT_FAILURE);
   }
   prepare_connect(server_name, port, mode);
@@ -79,11 +80,12 @@ void prepare_connect(const char *server_name, int port, const char *mode){
   /*  Ziskani adresy serveru pomoci DNS */
   server = gethostbyname(server_name);
   if (server == NULL) {
-      fprintf(stderr,"ERROR: no such host as %s\n", server_name);
+      fprintf(stderr,"ERR: Neexistujici host %s\n", server_name);
       exit(EXIT_FAILURE);
   }
 
   /*  Nalezeni IP adresy serveru a inicializace struktury server_address */
+  /*  využito z https://git.fit.vutbr.cz/NESFIT/IPK-Projekty/src/branch/master/Stubs/cpp/DemoUdp/client.c */
   bzero((char *) &server_address, sizeof(server_address));
   server_address.sin_family = AF_INET;
   bcopy((char *)server->h_addr, (char *)&server_address.sin_addr.s_addr, server->h_length);
@@ -93,7 +95,7 @@ void prepare_connect(const char *server_name, int port, const char *mode){
   if(strcmp(mode, "udp") == 0){
     client_socket = socket(AF_INET, SOCK_DGRAM, 0);
     if(client_socket < 1){
-      perror("ERROR: Chyba pri vytvareni socketu");
+      perror("ERR: Chyba pri vytvareni socketu");
       exit(EXIT_FAILURE);
     }
     flag = 1;
@@ -104,7 +106,7 @@ void prepare_connect(const char *server_name, int port, const char *mode){
   else{
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if(client_socket < 1){
-      perror("ERROR: Chyba pri vytvareni socketu");
+      perror("ERR: Chyba pri vytvareni socketu");
       exit(EXIT_FAILURE);
     }
     flag = 2;
@@ -114,6 +116,7 @@ void prepare_connect(const char *server_name, int port, const char *mode){
 
 /* Komunikace se serverem pomoci protokolu UDP*/
 void do_udp(struct sockaddr_in* server_address){
+
   /*  Pomocne promenne pro komunikaci se serverem */
   socklen_t serverlen;
   char input[PAYLOADSIZE];
@@ -135,15 +138,14 @@ void do_udp(struct sockaddr_in* server_address){
     memcpy(send + 2, input, 255);
 
     /* Odeslani zpravy na server */
-    serverlen = sizeof(*server_address);
-    bytes_sent = sendto(client_socket, send, strlen(&send[2]) + 2, 0, (struct sockaddr *)server_address, serverlen);
+    bytes_sent = sendto(client_socket, send, strlen(&send[2]) + 2, 0, (struct sockaddr *)server_address, sizeof(*server_address));
     if (bytes_sent < 0) 
-      perror("ERROR: Chyba pri odesilani na server");
+      perror("ERR: Chyba pri odesilani na server");
     
     /* Prijeti odpovedi*/
     bytes_recv = recvfrom(client_socket, recv, 257, 0, (struct sockaddr *)server_address, &serverlen);
     if (bytes_recv < 0) 
-      perror("ERROR: Chyba pri prijimani odpovedi ze serveru");
+      perror("ERR: Chyba pri prijimani odpovedi ze serveru");
 
     /* Vypsani odpovedi*/
     memcpy(strip, &recv[3], recv[2]); //lze pouzit strlen(&send[3]) pro delku zpravy - predpokladam spravnou odpoved serveru
@@ -157,7 +159,7 @@ void do_udp(struct sockaddr_in* server_address){
 void do_tcp(struct sockaddr_in* server_address){
   int connection = connect(client_socket, (const struct sockaddr *)server_address, sizeof(*server_address));
   if (connection != 0){
-    perror("ERROR: Chyba pri pripojovani k serveru (TCP)");
+    perror("ERR: Chyba pri pripojovani k serveru (TCP)");
     exit(EXIT_FAILURE);        
   }
 
@@ -173,14 +175,14 @@ void do_tcp(struct sockaddr_in* server_address){
     /* odeslani zpravy na server */
     bytes_sent = send(client_socket, input, strlen(input), 0);
     if (bytes_sent < 0) 
-      perror("ERROR in sendto");
+      perror("ERR: Chyba pri odesilani dat na server");
     
     bzero(input, TCPSIZE);
 
     /* prijeti odpovedi a jeji vypsani */
     bytes_recv = recv(client_socket, input, TCPSIZE, 0);
     if (bytes_recv < 0) 
-      perror("ERROR in recvfrom");
+      perror("ERR: Chyba pri prijimani dat ze serveru");
       
     printf("%s", input);
 
@@ -201,7 +203,7 @@ void leave(int s){
 
     bytes = send(client_socket, input, strlen(input), 0);
     if (bytes < 0) 
-      perror("ERROR in recvfrom");
+      perror("ERR: Chyba pri odesilani dat na server");
 
     printf("%s", input);
 
@@ -210,7 +212,7 @@ void leave(int s){
 
     bytes = recv(client_socket, input, PAYLOADSIZE, 0);
     if (bytes < 0) 
-      perror("ERROR in recvfrom");
+      perror("ERR: Chyba pri prijimani dat ze serveru");
 
     printf("%s", input);
     shutdown(client_socket, SHUT_RDWR);
